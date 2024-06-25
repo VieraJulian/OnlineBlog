1de1b77d09674c2330f3ffa29dbc1e6d5d9ee96c
VieraJulian <julianviera2019@hotmail.com>
2024-06-25 19:53:03 -0300

    Precargando roles y permisos en la base de datos

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO permissions (permission_name) values ("READ"), ("CREATE"), ("UPDATE"), ("DELETE");
+INSERT INTO roles (role) values ("ADMIN"), ("AUTHOR"), ("USER");
+INSERT INTO roles_permissions (permission_id, role_id) values (1,1), (2,1), (3,1), (4,1),
+                                                              (1,2), (2,2), (3,2), (4,2),
+                                                              (1,3);
