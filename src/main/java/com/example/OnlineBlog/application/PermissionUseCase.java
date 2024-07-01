2add42c87c07f81f0d6a17744162eece0bbc8adc
VieraJulian <julianviera2019@hotmail.com>
2024-07-01 09:36:46 -0300

    Casos de usos para permisos terminados

--- src/main/java/com/example/OnlineBlog/application/PermissionUseCase.java
+++ src/main/java/com/example/OnlineBlog/application/PermissionUseCase.java
@@ -1,6 +1,7 @@
 package com.example.OnlineBlog.application;
 
 import com.example.OnlineBlog.domain.Permission;
+import com.example.OnlineBlog.domain.Post;
 import com.example.OnlineBlog.infrastructure.inputPort.IPermissionInputPort;
 import com.example.OnlineBlog.infrastructure.outputPort.IPermissionMethod;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,26 +18,31 @@ public class PermissionUseCase implements IPermissionInputPort {
 
     @Override
     public Permission save(Permission permission) {
-        return null;
+        return permissionMethod.save(permission);
     }
 
     @Override
     public Permission update(Long id, Permission permission) {
+        Optional<Permission> permissionDB = permissionMethod.findById(id);
+
+        permissionDB.ifPresent(perm -> perm.setPermissionName(permission.getPermissionName()));
+
         return null;
     }
 
     @Override
     public Optional<Permission> findById(Long id) {
-        return Optional.empty();
+        return permissionMethod.findById(id);
     }
 
     @Override
     public List<Permission> permissionsList() {
-        return List.of();
+        return permissionMethod.findAll();
     }
 
     @Override
     public String deleteById(Long id) {
-        return "";
+        permissionMethod.deleteById(id);
+        return "Permission deleted successfully.";
     }
 }
