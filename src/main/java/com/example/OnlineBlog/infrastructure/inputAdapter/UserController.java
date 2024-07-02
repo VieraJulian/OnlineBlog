00dc01767557ca76d94c9e5f929f572dcf861a79
VieraJulian <julianviera2019@hotmail.com>
2024-07-02 16:44:22 -0300

    Empezando con los controladores de users

--- src/main/java/com/example/OnlineBlog/infrastructure/inputAdapter/UserController.java
+++ src/main/java/com/example/OnlineBlog/infrastructure/inputAdapter/UserController.java
@@ -1,12 +1,64 @@
 package com.example.OnlineBlog.infrastructure.inputAdapter;
 
+import com.example.OnlineBlog.domain.UserEntity;
 import com.example.OnlineBlog.infrastructure.inputPort.IUserInputPort;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private IUserInputPort userInputPort;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserEntity> getUserById(@PathVariable Long id){
+        try {
+            Optional<UserEntity> user = userInputPort.findById(id);
+            return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user){
+        try {
+            UserEntity userNew = userInputPort.save(user);
+            return new ResponseEntity<>(userNew, HttpStatus.CREATED);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<UserEntity> updateUser(@PathVariable Long id, @RequestBody UserEntity user){
+        try {
+            UserEntity userUpdated = userInputPort.update(id, user);
+            return new ResponseEntity<>(userUpdated, HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id){
+        try {
+            String msg = userInputPort.deleteById(id);
+            return new ResponseEntity<>(msg, HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
